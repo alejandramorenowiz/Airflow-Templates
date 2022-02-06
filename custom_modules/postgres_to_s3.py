98f5c00efce729abf62b93d2a08c456cb4eb08ab
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-02-06 03:09:12 -0600

    Update postgres_to_s3.py

--- custom_modules/postgres_to_s3.py
+++ custom_modules/postgres_to_s3.py
@@ -2,9 +2,10 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.amazon.aws.utils.redshift import build_credentials_block
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from typing import List, Optional, Union
+
+AVAILABLE_METHODS = ['APPEND', 'REPLACE', 'UPSERT']
 
 import pandas as pd
 
