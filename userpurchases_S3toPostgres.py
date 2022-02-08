d80699101a6235227dd6743073d36a487a0a2470
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-02-07 20:13:31 -0600

    Update userpurchases_S3toPostgres.py

--- userpurchases_S3toPostgres.py
+++ userpurchases_S3toPostgres.py
@@ -10,8 +10,8 @@ from custom_modules.s3_to_postgres import S3ToPostgresTransfer
 default_args = {
     'owner': 'alejandra.moreno',
     'depends_on_past': False,        
-    'start_date'=datetime(2022, 1, 1),
-     catchup=False
+    'start_date': airflow.utils.dates.days_ago(1),
+    catchup=False
 }
 
 dag = DAG('dag_insert_userpurchases_s3toPostgres', 
