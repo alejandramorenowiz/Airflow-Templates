f7ca8517be49d493feaaaf7119189eff3c531f28
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-02-07 22:18:33 -0600

    Update userpurchases_S3toPostgres.py

--- userpurchases_S3toPostgres.py
+++ userpurchases_S3toPostgres.py
@@ -13,7 +13,7 @@ default_args = {
     'owner': 'alejandra.moreno',
     'depends_on_past': False, 
     'schedule_interval': '@once',
-    'start_date':datetime(2022, 1, 1),
+    'start_date': airflow.utils.dates.days_ago(1),
     'catchup':False
 }
 
