c978435a4f0e9d0dc60b5817a30e5d56a25de77f
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-02-17 13:14:50 -0600

    Add files via upload

---
+++ dags/movie_review_classification.py
@@ -0,0 +1,85 @@
+import sys
+import os.path
+import io
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.exceptions import AirflowException
+import airflow.utils.dates
+
+from airflow.providers.amazon.aws.operators.emr_containers import EMRContainerOperator
+
+
+# Stopper  - How to save environment variables
+# [START emr_eks_env_variables]
+VIRTUAL_CLUSTER_ID = os.getenv("VIRTUAL_CLUSTER_ID", "test-cluster")
+JOB_ROLE_ARN = os.getenv("JOB_ROLE_ARN", "arn:aws:iam::012345678912:role/emr_eks_default_role")
+
+c = BaseHook.get_connection("emr_eks")
+cluster_args = c.extra_dejson
+# [END emr_eks_env_variables]
+
+
+# [START emr_eks_config]
+JOB_DRIVER_ARG = {
+    "sparkSubmitJobDriver": {
+        "entryPoint": "s3://spark-jobscripts/movie_review_logic.py",
+        "sparkSubmitParameters": "--conf spark.executors.instances=1"
+                                 " --conf spark.executors.memory=2G"
+                                 " --conf spark.executor.cores=1"
+                                 " --conf spark.driver.cores=1"
+    }
+}
+
+CONFIGURATION_OVERRIDES_ARG = {
+    "applicationConfiguration": [
+        {
+            "classification": "spark-defaults",
+            "properties": {
+                "spark.hadoop.hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory",  # noqa: E501
+                #OR
+                #"spark.dynamicAllocation.enabled": "false", "spark.kubernetes.executor.deleteOnTermination": "true", "spark.kubernetes.container.image": #spark_image, "spark.hadoop.fs.s3a.multiobjectdelete.enable": "false"
+            },
+        }
+    ],
+    "monitoringConfiguration": {
+        "cloudWatchMonitoringConfiguration": {
+            "logGroupName": "/aws/emr-eks-spark",
+            "logStreamNamePrefix": "airflow",
+        }
+        #OR
+        # "s3MonitoringConfiguration": {
+        #    "logUri": "s3://spark-test"
+        #}
+    },
+}
+# [END emr_eks_config]
+
+default_args = {
+    'owner': 'alejandra.moreno',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(1)
+}
+
+dag = DAG('dag__moviereview_job', 
+        default_args = default_args,
+        description='Executes movie review logic',
+        schedule_interval='@once',        
+        catchup=False)
+
+movie_review_job = EMRContainerOperator(
+                            task_id = 'dag_movie_review_job',
+                            virtual_cluster_id=cluster_args.get("virtual_cluster_id"),
+                            #virtual_cluster_id=VIRTUAL_CLUSTER_ID,
+                            execution_role_arn=cluster_args.get("job_role_arn"),
+                            #execution_role_arn=JOB_ROLE_ARN,                   
+                            release_label="emr-6.3.0-latest",
+                            job_driver=JOB_DRIVER_ARG,
+                            configuration_overrides=CONFIGURATION_OVERRIDES_ARG,
+                            name="movie_reviews.py"
+                            dag = dag
+)
+
+movie_review_job
+# https://airflow.apache.org/docs/apache-airflow-providers-amazon/2.2.0/operators/emr_eks.html#prerequisite-tasks
+# https://airflow.apache.org/docs/apache-airflow-providers-amazon/2.2.0/_modules/airflow/providers/amazon/aws/example_dags/example_emr_eks_job.html
---
+++ dags/userpurchases_S3toPostgres.py
@@ -0,0 +1,37 @@
+import sys
+import os.path
+import io
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.exceptions import AirflowException
+import airflow.utils.dates
+
+# Stopper how to upload custom modules
+#sys.path.append("/opt/airflow/dags/custom_modules")
+from custom_modules.s3_to_postgres import S3ToPostgresTransfer
+
+default_args = {
+    'owner': 'alejandra.moreno',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(1)
+}
+
+dag = DAG('dag_insert_userpurchases_s3toPostgres', 
+        default_args = default_args,
+        description='Insert Data from CSV in S3 To Postgres',
+        schedule_interval='@once',        
+        catchup=False)
+
+s3_to_postgres_operator = S3ToPostgresTransfer(
+                            task_id = 'dag_s3_to_postgres',
+                            schema =  'bootcampdb',
+                            table= 'user_purchases',
+                            s3_bucket = 'customerbucketam',
+                            s3_key =  'user_purchase.csv',
+                            aws_conn_postgres_id = 'postgres_default',
+                            aws_conn_id = 'aws_default',   
+                            dag = dag
+)
+
+s3_to_postgres_operator
