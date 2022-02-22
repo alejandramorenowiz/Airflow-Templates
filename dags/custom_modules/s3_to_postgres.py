9a8f8317fa0f97b7c2c93a3f5ced4cf19d8f95de
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-02-22 02:34:23 -0600

    Update s3_to_postgres.py

--- dags/custom_modules/s3_to_postgres.py
+++ dags/custom_modules/s3_to_postgres.py
@@ -1,3 +1,11 @@
+import sys
+from datetime import datetime
+import os.path
+import pandas as pd
+import io
+
+
+
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.hooks.S3_hook import S3Hook
 from airflow.models import BaseOperator 
@@ -5,9 +13,6 @@ from airflow.utils.decorators import apply_defaults
 from airflow.exceptions import AirflowException
 from typing import List, Optional, Union
 
-import os.path
-import pandas as pd
-import io
 
 
 def print_welcome():
