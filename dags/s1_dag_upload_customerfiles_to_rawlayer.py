3806f43103181a0e89a043bc55cfb518c44e1bed
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-02 10:36:11 -0600

    Update s1_dag_upload_customerfiles_to_rawlayer.py

--- dags/s1_dag_upload_customerfiles_to_rawlayer.py
+++ dags/s1_dag_upload_customerfiles_to_rawlayer.py
@@ -29,7 +29,7 @@ user_purchase_raw_transfer = S3CopyObjectOperator(
                             dest_bucket_key='user_purchase.csv',
                             aws_conn_id='aws_default',
                             source_bucket_name='customerbucketam',
-                            dest_bucket_name='raw-layer20220301034958761700000005', 
+                            dest_bucket_name='raw-layer20220302064717029600000005', 
                             dag = dag
 )
 
@@ -39,7 +39,7 @@ moview_review_raw_transfer = S3CopyObjectOperator(
                             dest_bucket_key='movie_review.csv',
                             aws_conn_id='aws_default',
                             source_bucket_name='customerbucketam',
-                            dest_bucket_name='raw-layer20220301034958761700000005', 
+                            dest_bucket_name='raw-layer20220302064717029600000005', 
                             dag = dag
 )
 
@@ -49,7 +49,7 @@ log_review_raw_transfer = S3CopyObjectOperator(
                             dest_bucket_key='log_reviews.csv',
                             aws_conn_id='aws_default',
                             source_bucket_name='customerbucketam',
-                            dest_bucket_name='raw-layer20220301034958761700000005', 
+                            dest_bucket_name='raw-layer20220302064717029600000005', 
                             dag = dag
 )
 
