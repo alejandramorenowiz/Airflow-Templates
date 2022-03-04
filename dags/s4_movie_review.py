41c35e06825a8f3cf95a2912ca40ccde60b5d008
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-03 23:10:15 -0600

    Add files via upload

---
+++ dags/s4_movie_review.py
@@ -0,0 +1,101 @@
+import airflow.utils.dates
+from airflow import DAG
+
+from custom_dags.dag_s3_to_postgres import S3ToPostgresTransfer
+from airflow.contrib.operators.emr_create_job_flow_operator import EmrCreateJobFlowOperator
+from airflow.contrib.sensors.emr_job_flow_sensor import EmrJobFlowSensor
+from airflow.contrib.sensors.emr_step_sensor import  EmrStepSensor
+
+
+
+BUCKET_NAME = "customerbucketam"
+#s3_data = "bronze/movie_review.csv"
+#s3_script = "dags/scripts/process_movie_review.py"
+#s3_clean = "silver/reviews/"
+logs_location = "logs"
+
+SPARK_STEPS = [ 
+    {
+        "Name": "Process silver data",
+        "ActionOnFailure": "CANCEL_AND_WAIT",
+        "HadoopJarStep": {
+            "Jar": "command-runner.jar",
+            "Args": [
+                "spark-submit",
+                "--deploy-mode",
+                "client",
+                f"s3://spark-jobscripts/movie_review_logic.py",
+            ],
+        },
+    }
+]
+
+
+JOB_FLOW_OVERRIDES = {
+    "Name": "Process movie review",
+    "ReleaseLabel": "emr-6.5.0",
+    "Applications": [{"Name": "Hadoop"}, {"Name": "Spark"}], # We want our EMR cluster to have HDFS and Spark
+    "LogUri" : f"s3://{BUCKET_NAME}/{logs_location}",
+    "Configurations": [
+        {
+            "Classification": "spark-env",
+            "Configurations": [
+                {
+                    "Classification": "export",
+                    "Properties": {"PYSPARK_PYTHON": "/usr/bin/python3"}, # by default EMR uses py2, change it to py3
+                }
+            ],
+        }
+    ],
+    "Instances": {
+        "InstanceGroups": [
+            {
+                "Name": "Master node",
+                "Market": "SPOT",
+                "InstanceRole": "MASTER",
+                "InstanceType": "m1.medium",
+                "InstanceCount": 1,
+            },
+            {
+                "Name": "Core - 2",
+                "Market": "SPOT", # Spot instances are a "use as available" instances
+                "InstanceRole": "CORE",
+                "InstanceType": "m1.medium",
+                "InstanceCount": 2,
+            },
+        ],
+        "KeepJobFlowAliveWhenNoSteps": False,
+        "TerminationProtected": False
+    },
+    'Steps': SPARK_STEPS,
+    "JobFlowRole": "EMR_EC2_DefaultRole",
+    "ServiceRole": "EMR_DefaultRole",
+}
+
+default_args = {
+    'owner': 'alejandra.moreno',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(1)
+}
+
+
+dag = DAG('s44_dag__moviereview_job', 
+        default_args = default_args,
+        description='Executes movie review logic',
+        schedule_interval='@once',        
+        catchup=False)
+
+create_emr_cluster = EmrCreateJobFlowOperator(
+    task_id="create_emr_cluster",
+    job_flow_overrides=JOB_FLOW_OVERRIDES,
+    aws_conn_id="aws_default",
+    emr_conn_id="emr_default",
+    dag=dag,
+)
+
+job_sensor = EmrJobFlowSensor(task_id='check_job_flow',
+ job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster', key='return_value') }}",
+ dag = dag)
+
+
+create_emr_cluster >> job_sensor
