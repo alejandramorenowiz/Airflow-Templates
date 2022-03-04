628ed8c20f2e5096dbb9337d5c34830c521688f3
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-04 12:25:32 -0600

    Update s4_movie_review.py

--- dags/s4_movie_review.py
+++ dags/s4_movie_review.py
@@ -7,34 +7,10 @@ from airflow.contrib.sensors.emr_step_sensor import  EmrStepSensor
 
 
 
-BUCKET_NAME = "customerbucketam"
-#s3_data = "bronze/movie_review.csv"
-#s3_script = "dags/scripts/process_movie_review.py"
-#s3_clean = "silver/reviews/"
-logs_location = "logs"
-
-SPARK_STEPS = [ 
-    {
-        "Name": "Process silver data",
-        "ActionOnFailure": "CANCEL_AND_WAIT",
-        "HadoopJarStep": {
-            "Jar": "command-runner.jar",
-            "Args": [
-                "spark-submit",
-                "--deploy-mode",
-                "client",
-                f"s3://spark-jobscripts/regex.py",
-            ],
-        },
-    }
-]
-
-
 JOB_FLOW_OVERRIDES = {
-    "Name": "Process movie review",
-    "ReleaseLabel": "emr-6.5.0",
+    "Name": "Movie review classifier",
+    "ReleaseLabel": "emr-5.29.0",
     "Applications": [{"Name": "Hadoop"}, {"Name": "Spark"}], # We want our EMR cluster to have HDFS and Spark
-    "LogUri" : f"s3://{BUCKET_NAME}/{logs_location}",
     "Configurations": [
         {
             "Classification": "spark-env",
@@ -52,49 +28,31 @@ JOB_FLOW_OVERRIDES = {
                 "Name": "Master node",
                 "Market": "SPOT",
                 "InstanceRole": "MASTER",
-                "InstanceType": "m1.medium",
+                "InstanceType": "m4.xlarge",
                 "InstanceCount": 1,
             },
             {
                 "Name": "Core - 2",
                 "Market": "SPOT", # Spot instances are a "use as available" instances
                 "InstanceRole": "CORE",
-                "InstanceType": "m1.medium",
+                "InstanceType": "m4.xlarge",
                 "InstanceCount": 2,
             },
         ],
-        "KeepJobFlowAliveWhenNoSteps": False,
-        "TerminationProtected": False
+        "KeepJobFlowAliveWhenNoSteps": True,
+        "TerminationProtected": False, # this lets us programmatically terminate the cluster
     },
-    'Steps': SPARK_STEPS,
     "JobFlowRole": "EMR_EC2_DefaultRole",
     "ServiceRole": "EMR_DefaultRole",
 }
 
-default_args = {
-    'owner': 'alejandra.moreno',
-    'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(1)
-}
-
-
-dag = DAG('s44_dag__moviereview_job', 
-        default_args = default_args,
-        description='Executes movie review logic',
-        schedule_interval='@once',        
-        catchup=False)
-
+# Create an EMR cluster
 create_emr_cluster = EmrCreateJobFlowOperator(
     task_id="create_emr_cluster",
     job_flow_overrides=JOB_FLOW_OVERRIDES,
     aws_conn_id="aws_default",
-    emr_conn_id="emr_default",
+    emr_conn_id="aws_default",
     dag=dag,
 )
 
-job_sensor = EmrJobFlowSensor(task_id='check_job_flow',
- job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster', key='return_value') }}",
- dag = dag)
-
-
-create_emr_cluster >> job_sensor
+create_emr_cluster
