df8140cd20b81af977f93b7dc9443289918491c0
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-08 03:09:06 -0600

    Update s4_movie_review.py

--- dags/s4_movie_review.py
+++ dags/s4_movie_review.py
@@ -7,10 +7,9 @@ from airflow.contrib.sensors.emr_step_sensor import  EmrStepSensor
 from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
 from airflow.contrib.operators.emr_terminate_job_flow_operator import EmrTerminateJobFlowOperator
 
-
 BUCKET_NAME = "customerbucketam"
 s3_data = "movie_review.csv"
-s3_script = "s3://spark-jobscripts/regex.py"
+s3_script = "s3://spark-jobscripts/test.py"
 s3_clean = "processed/reviews/"
 logs_location = "logs"
 
@@ -23,14 +22,31 @@ default_args = {
 dag = DAG('s4_dag_movie_', 
         default_args = default_args,
         description='Executes movie review logic',
-        schedule_interval='@once',        
-        catchup=False)
+        schedule_interval='@once')
+
+SPARK_STEPS = [ # Note the params values are supplied to the operator
+   
+    {
+        "Name": "Classify movie reviews",
+        "ActionOnFailure": "CANCEL_AND_WAIT",
+        "HadoopJarStep": {
+            "Jar": "command-runner.jar",
+            "Args": [
+                "spark-submit",
+                "--deploy-mode",
+                "client",
+                "s3://spark-jobscripts/test.py",
+            ],
+        },
+    }  
+]
 
 
 JOB_FLOW_OVERRIDES = {
     "Name": "Movie review classifier",
-    "ReleaseLabel": "emr-5.29.0",
+    "ReleaseLabel": "emr-6.5.0",
     "Applications": [{"Name": "Hadoop"}, {"Name": "Spark"}], # We want our EMR cluster to have HDFS and Spark
+    "LogUri" : f"s3://{BUCKET_NAME}/{logs_location}",
     "Configurations": [
         {
             "Classification": "spark-env",
@@ -48,20 +64,21 @@ JOB_FLOW_OVERRIDES = {
                 "Name": "Master node",
                 "Market": "SPOT",
                 "InstanceRole": "MASTER",
-                "InstanceType": "m4.xlarge",
+                "InstanceType": "m1.medium",
                 "InstanceCount": 1,
             },
             {
                 "Name": "Core - 2",
                 "Market": "SPOT", # Spot instances are a "use as available" instances
                 "InstanceRole": "CORE",
-                "InstanceType": "m4.xlarge",
+                "InstanceType": "m1.medium",
                 "InstanceCount": 2,
             },
         ],
-        "KeepJobFlowAliveWhenNoSteps": True,
+        "KeepJobFlowAliveWhenNoSteps": False,
         "TerminationProtected": False, # this lets us programmatically terminate the cluster
     },
+    'Steps': SPARK_STEPS,
     "JobFlowRole": "EMR_EC2_DefaultRole",
     "ServiceRole": "EMR_DefaultRole",
 }
@@ -74,34 +91,6 @@ create_emr_cluster = EmrCreateJobFlowOperator(
     emr_conn_id="aws_default",
     dag=dag,
 )
-SPARK_STEPS = [ # Note the params values are supplied to the operator
-   
-    {
-        "Name": "Classify movie reviews",
-        "ActionOnFailure": "CANCEL_AND_WAIT",
-        "HadoopJarStep": {
-            "Jar": "command-runner.jar",
-            "Args": [
-                "spark-submit",
-                "--deploy-mode",
-                "client",
-                "s3://spark-jobscripts/regex.py",
-            ],
-        },
-    },
-    {
-        "Name": "Move clean data from HDFS to S3",
-        "ActionOnFailure": "CANCEL_AND_WAIT",
-        "HadoopJarStep": {
-            "Jar": "command-runner.jar",
-            "Args": [
-                "s3-dist-cp",
-                "--src=/output",
-                "--dest=s3://{{ params.BUCKET_NAME }}/{{ params.s3_clean }}",
-            ],
-        },
-    },
-]
 
 # Add your steps to the EMR cluster
 step_adder = EmrAddStepsOperator(
@@ -118,27 +107,11 @@ step_adder = EmrAddStepsOperator(
     dag=dag,
 )
 
-last_step = len(SPARK_STEPS) - 1 # this value will let the sensor know the last step to watch
-
-# wait for the steps to complete
-step_checker = EmrStepSensor(
-    task_id="watch_step",
-    job_flow_id="{{ task_instance.xcom_pull('create_emr_cluster', key='return_value') }}",
-    step_id="{{ task_instance.xcom_pull(task_ids='add_steps', key='return_value')["
-    + str(last_step)
-    + "] }}",
-    aws_conn_id="aws_default",
-    dag=dag,
-)
-
-# Terminate the EMR cluster
-terminate_emr_cluster = EmrTerminateJobFlowOperator(
-    task_id="terminate_emr_cluster",
-    job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster', key='return_value') }}",
-    aws_conn_id="aws_default",
-    dag=dag,
-)
+job_sensor = EmrJobFlowSensor(task_id='check_job_flow',
+ job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster', key='return_value') }}",
+ dag = dag)
 
 
 
-create_emr_cluster >> step_adder >> step_checker >> terminate_emr_cluster
+create_emr_cluster >> job_sensor
+#create_emr_cluster >> step_adder >> step_checker >> terminate_emr_cluster
