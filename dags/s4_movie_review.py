cb0d70b81a8d5ab7fe93655407631e75d5cfeca4
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-08 03:12:53 -0600

    Update s4_movie_review.py

--- dags/s4_movie_review.py
+++ dags/s4_movie_review.py
@@ -88,7 +88,7 @@ create_emr_cluster = EmrCreateJobFlowOperator(
     task_id="create_emr_cluster",
     job_flow_overrides=JOB_FLOW_OVERRIDES,
     aws_conn_id="aws_default",
-    emr_conn_id="aws_default",
+    emr_conn_id="emr_default",
     dag=dag,
 )
 
