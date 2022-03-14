788e59114aa3c536d503ee51efe8fb28fb3e496f
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-14 17:21:50 -0600

    Update and rename s3_insert_userpurchases_s3toPostgres to s3_insert_userpurchases_Postgrestos3.py

--- dags/s3_insert_userpurchases_Postgrestos3.py
+++ dags/s3_insert_userpurchases_Postgrestos3.py
@@ -123,7 +123,7 @@ default_args = {
     'schedule_interval': 'None'
 }
 
-dag = DAG('s3_dag_insert_userpurchases_s3toPostgres', 
+dag = DAG('s3_dag_insert_userpurchases_Postgrestos3', 
         default_args = default_args,
         description='Insert Data from Postgres to S3',
         schedule_interval='@once',        
