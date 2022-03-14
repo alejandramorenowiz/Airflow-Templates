cc2d53b380ac0eafac0e4fc1b47db2f7f66f85eb
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-13 23:00:06 -0600

    Update sx_dag_redshift.py

--- dags/sx_dag_redshift.py
+++ dags/sx_dag_redshift.py
@@ -40,8 +40,8 @@ default_args = {
 
 dag = DAG('sx_dag_redshift', default_args = default_args, schedule_interval = '@daily')
 
-setup_task_create_tables = RedshiftSQLOperator(
-        redshift_conn_id="redshift_default",
+setup_task_create_tables = PostgresOperator((
+        postgres_conn_id='redshift_default',
         task_id='setup_task_create_table',
         sql= _query,
         autocommit = True,
