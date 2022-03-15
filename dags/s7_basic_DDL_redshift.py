739da99004cc001371ff27d2d782f502e03c45c2
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-14 21:58:52 -0600

    Update s7_basic_DDL_redshift.py

--- dags/s7_basic_DDL_redshift.py
+++ dags/s7_basic_DDL_redshift.py
@@ -57,11 +57,11 @@ _query = ["""
                       LOCATION 's3://staging-layer20220307050201862200000005/user_purchase_data_from_postgres.parquet/';
                       """,
                       
-                      """
-                      create external table fma_schema.dim_os(
-                        id_os varchar(32) NOT NULL,            
-                        os varchar(256),
-                        CONSTRAINT idos_pkey PRIMARY KEY (id_os)
+                      """                    
+                      CREATE TABLE IF NOT EXISTS fma_schema.dim_os (
+                      id_os varchar(32) NOT NULL,
+                      os varchar(256),
+                      CONSTRAINT idos_pkey PRIMARY KEY (id_os)
                       );
                       """]
 
