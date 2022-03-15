fab4934104db9fed1950284378f31bb7a3e91e5f
Alejandra Moreno <97913544+alejandramorenowiz@users.noreply.github.com>
2022-03-15 00:37:25 -0600

    Update s7_basic_DDL_redshift.py

--- dags/s7_basic_DDL_redshift.py
+++ dags/s7_basic_DDL_redshift.py
@@ -53,8 +53,11 @@ _query = ["""
                         customer_id numeric,
                         country varchar
                       )
-                      stored as PARQUET
-                      LOCATION 's3://staging-layer20220307050201862200000005/user_purchase_data_from_postgres.parquet/';
+                      row format delimited
+                      fields terminated by ','
+                      stored as textfile
+                      location 's3://staging-layer20220307050201862200000005/user_purchase_data_from_postgres.json/'
+                      table properties ('skip.header.line.count'='1');
                       """]
 
 
